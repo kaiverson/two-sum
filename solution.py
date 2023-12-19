0ac64c4eb56f3fed69ef0382e26c467c530f35dd
Kai Iverson <kaiiverson510@gmail.com>
2023-12-18 16:59:56 -0900

    Add solution to Two Sum problem and make tests better

--- solution.py
+++ solution.py
@@ -1,3 +1,14 @@
+from typing import List
+
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
+    numsMap = {}
+
+    for i in range(len(nums)):
+      compliment = target - nums[i]
+      if (compliment in numsMap):
+        return [numsMap[compliment], i]
+      numsMap[nums[i]] = i
+
+    # Program should never reach this point for inputs within constraints
+    return []
--- test_two_sum.py
+++ test_two_sum.py
@@ -1,27 +1,37 @@
 import unittest
 from solution import Solution
 
+def compareAnswers(answer, expected):
+  print(f"\n****************\nOutput:   {answer}\nExpected: {expected}\n****************")
+
+
 class TestTwoSum(unittest.TestCase):
   def test_1(self):
     instance = Solution()
     nums = [2, 7, 11, 15]
     target = 9
-    solution = [0, 1]
-    self.assertEqual(instance.twoSum(nums, target), expected)
+    expected = [0, 1]
+    answer = instance.twoSum(nums, target)
+    compareAnswers(answer, expected)
+    self.assertCountEqual(answer, expected)
 
-def test_2(self):
+  def test_2(self):
     instance = Solution()
     nums = [3, 2, 4]
     target = 6
-    solution = [1, 2]
-    self.assertEqual(instance.twoSum(nums, target), expected)
+    expected = [1, 2]
+    answer = instance.twoSum(nums, target)
+    compareAnswers(answer, expected)
+    self.assertCountEqual(answer, expected)
 
-def test_3(self):
+  def test_3(self):
     instance = Solution()
     nums = [3, 3]
     target = 6
-    solution = [0, 1]
-    self.assertEqual(instance.twoSum(nums, target), expected)
+    expected = [0, 1]
+    answer = instance.twoSum(nums, target)
+    compareAnswers(answer, expected)
+    self.assertCountEqual(answer, expected)
 
 
 if __name__=="__main__":
